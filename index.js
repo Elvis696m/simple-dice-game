dad630b799ba56c661214544caaea4c3d4817f24
Elvis696m <elvice218@gmail.com>
2026-08-15 22:56:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ document.getElementById('myButton').onclick=function(){
 let guess=document.getElementById('guessField').value
 guesses+=1
 if(guess==answer){
-   alert('You win!')
+    alert('You won!')
 }
+else if(guesses>=3){
+    alert('You are Out.Try harder next time!')
+}
+
 else{
-   alert('Try again!')
+    alert('Try again!')
 }
+
 }
