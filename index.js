396f3ced24ee6fdfdf4aa866083da0d0fdcb1033
Elvis696m <elvice218@gmail.com>
2026-08-18 16:24:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-let answer = Math.floor(Math.random()*6+1);
-guesses=0
+setTimeout(function () {
+    document.getElementById('intro').style.display = 'none';
+}, 2000)
+let answer = Math.floor(Math.random() * 6 + 1);
+guesses = 0
 
-document.getElementById('myButton').onclick=function(){
-let guess=document.getElementById('guessField').value
-guesses+=1
-if(guess==answer){
-    alert('You won!')
-}
-else if(guess>6){
-    alert('Out of Range!')
-}
-else if(guesses>=3){
-    alert('You are Out.Try harder next time!')
-}
+document.getElementById('myButton').onclick = function () {
+    let guess = document.getElementById('guessField').value
+    guesses += 1
+    if (guess == answer) {
+        alert('You won!')
+    }
+    else if (guess > 6) {
+        alert('Out of Range!')
+    }
+    else if (guesses >= 3) {
+        alert('You are Out.Try harder next time!')
+    }
 
-else{
-    alert('Try again!')
-}
+    else {
+        alert('Try again!')
+    }
 
 }
