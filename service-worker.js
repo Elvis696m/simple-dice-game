c215763f2b3d1926502e892032b9eac8b2604f8d
Elvis696m <elvice218@gmail.com>
2026-09-06 14:05:01 +0300

    Remove music.mp3 from service worker cache
    Removed music.mp3 from the service worker cache.

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
     "./index.js",
     "./die.jpeg",
     "./dice pic.jpeg",
-    "./music.mp3",
+    
     "./manifest.json",
     "./icons/icon-192.png",
     "./icons/icon-512.png"
