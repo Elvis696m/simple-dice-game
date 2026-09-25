383efcdc829bcf3a4a7b6b6a75a040697566a297
Elvis696m <elvice218@gmail.com>
2026-09-25 16:33:51 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,6 @@ const FILES_TO_CACHE = [
     "./icons/icon-192.png",
     "./icons/icon-512.png"
 ];
-
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
